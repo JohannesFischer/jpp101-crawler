657bb8487819335f47d787d874f4226363a6ef25
Johannes Fischer <git@johannes-fischer.de>
2016-02-26 16:10:45 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,47 +1,41 @@
-var agent = require('superagent');
 var cheerio = require('cheerio');
+var request = require('request');
 
 var loginPage = 'http://www.japanesepod101.com/member/login_new.php';
 var episodeList = 'http://www.japanesepod101.com/index.php?cat=1';
-var loginData = {
-  amember_login: '',
-  amember_pass: ''
-};
+var loginData = {};
+var username = process.argv[2];
+var password = process.argv[3];
+var userAgent = 'Mozilla/5.0 (X11; Linux x86_64; rv:10.0) ' +
+                'Gecko/20100101 Firefox/10.0';
 
-agent
-  .get(loginPage)
-  .end(function(err, res) {
-    var $ = cheerio.load(res.text);
-    var loginAttemptId = $('form[name=login] input[name=login_attempt_id]');
-    loginData.login_attempt_id = loginAttemptId[0].attribs.value;
+request.get({
+  headers: {
+    'User-Agent': userAgent,
+  },
+  jar: true,
+  url: loginPage,
+}, function(error, response, body) {
+  var $ = cheerio.load(body);
+  $('input[name=amember_login]').val(username);
+  $('input[name=amember_pass]').val(password);
+  var formData = $('form[name=login]').serializeArray();
 
-    var requestId = $('form[name=login] input[name=request_id]');
-    loginData.request_id = loginAttemptId[0].attribs.value;
-
-    console.log(loginData);
-  });
-
-// agent
-//   .post(loginPage)
-//   .send(loginData)
-//   .end(function(err, res) {
-//     if (err || !res.ok) {
-//       console.log('Oh no! error');
-//     } else {
-//       //console.log('yay got ', res);
-//     }
-//   });
-
-/*
-agent
-.get(episodeList)
-.end(function(err, res) {
-  if (err || !res.ok) {
-    console.log('Oh no! error');
-  } else {
-    console.log('yay got ', res);
-    //   var $ = cheerio.load(body);
-    //   var list = $('div.ill-lessons-list');
-  }
+  console.log('trying to login using: ', formData);
+  login(formData);
 });
-*/
+
+function login(loginData) {
+  request.post({
+    form: loginData,
+    headers: {
+      'User-Agent': userAgent,
+    },
+    jar: true,
+    url: loginPage,
+  }, function(error, response, body) {
+    if (!error && response.statusCode == 200) {
+      console.log(response);
+    }
+  });
+}
