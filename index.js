42a456470b2f39adac01004b083c88249f0dc3ae
Johannes <git@johannes-fischer.de>
2016-03-27 21:39:40 +0900

    moved crawler logic to class, included prompt

--- index.js
+++ index.js
@@ -1,41 +1,84 @@
+var async = require('async');
 var cheerio = require('cheerio');
 var request = require('request');
+var cookieJar = request.jar();
+var prompt = require('cli-prompt');
+var Jpcrawler = require('./jpcrawler.js');
+var crawler = new Jpcrawler();
 
-var loginPage = 'http://www.japanesepod101.com/member/login_new.php';
-var episodeList = 'http://www.japanesepod101.com/index.php?cat=1';
-var loginData = {};
-var username = process.argv[2];
-var password = process.argv[3];
-var userAgent = 'Mozilla/5.0 (X11; Linux x86_64; rv:10.0) ' +
-                'Gecko/20100101 Firefox/10.0';
+var host = 'http://www.japanesepod101.com';
+var loginPage = host + '/member/login_new.php';
+var episodeList = host + '/index.php?cat=1';
+var lessonPage = host + '/index.php?cat=Introduction';
 
-request.get({
-  headers: {
-    'User-Agent': userAgent,
+// prompt to input username & password
+prompt.multi([
+  {
+    label: 'Enter your username',
+    key: 'username'
   },
-  jar: true,
-  url: loginPage,
-}, function(error, response, body) {
-  var $ = cheerio.load(body);
-  $('input[name=amember_login]').val(username);
-  $('input[name=amember_pass]').val(password);
-  var formData = $('form[name=login]').serializeArray();
+  {
+    label: 'Enter your password',
+    key: 'password',
+    type: 'password'
+  }
+], function(input) {
+  crawler.getPage(loginPage, function(error, response, body) {
+    var $ = cheerio.load(body);
+    $('input[name=amember_login]').val(input.username);
+    $('input[name=amember_pass]').val(input.password);
+    var formData = $('form[name=login]').serializeArray();
+    formData[3].value = formData[3].value.replace(' ', '+');
+    
+    var loginData = {};
+    formData.forEach(function(el) {
+      loginData[el.name] = el.value;
+    });
+    
+    console.log('Logging in as ' + input.username);
 
-  console.log('trying to login using: ', formData);
-  login(formData);
+    crawler.login(loginPage, loginData, response, function() {
+      crawler.getPage(lessonPage, function(error, response, body) {
+        // put available categories
+        var $ = cheerio.load(body);
+        var categories = $('a.ill-level-title');
+        if (categories.length > 0) {
+          console.log('Please select a category:');
+          for (var i = 0; i < categories.length; i++) {
+            console.log(`  [${i + 1}] ${categories[i].children[0].data}`); 
+          }
+          prompt('Put in the category number to continue: ', (input) => {
+            console.log('You picked ' + input);
+          });
+        }
+      });
+    });
+  });
 });
 
-function login(loginData) {
-  request.post({
-    form: loginData,
-    headers: {
-      'User-Agent': userAgent,
-    },
-    jar: true,
-    url: loginPage,
-  }, function(error, response, body) {
-    if (!error && response.statusCode == 200) {
-      console.log(response);
+var downloadList = [];
+
+var getEpisodeLinks = function(linkList, num) {
+  getPage(linkList[num], function(error, response, body) {
+    console.log('Grabbing episode page: ' + linkList[num]);
+    var $ = cheerio.load(body);
+    var downloadLink = $('a.media-download');
+    if (downloadLink !== undefined) {
+      for(var i = 0; i < downloadLink.length; i++) {
+        var attribs = downloadLink[i].attribs;
+
+        if (attribs.class.indexOf('locked') === -1) {
+          var target = downloadLink[i].attribs.href;
+          console.log('Lesson MP3 found: ', target);
+          downloadList.push(target);
+        }
+      }
+      
+      if (num < linkList.length) {
+        getEpisodeLinks(linkList, num + 1);
+      } else {
+        console.log('Finished grabbing links');
+      }
     }
   });
 }
---
+++ jpcrawler.js
@@ -0,0 +1,84 @@
+'use strict';
+
+var cheerio = require('cheerio');
+var request = require('request');
+var cookieJar = request.jar();
+
+class Jpcrawler {
+  
+  constructor() {
+    this.host = 'http://www.japanesepod101.com';
+    this.userAgent = 'Mozilla/5.0 (X11; Linux x86_64; rv:10.0) ' +
+                     'Gecko/20100101 Firefox/10.0';
+  }
+
+  followMetaRedirect(error, response, body, callback) {
+    if (!error && response.statusCode == 200) {
+      var $ = cheerio.load(body);
+      var regex = /<meta http-equiv="Refresh" CONTENT="1; URL=([^"]+)[^>]+>/;
+      var match = regex.exec(response.body);
+      
+      if (match !== null) {
+        this.getPage(this.host + match[1], function(error, response, body) {
+          var uri = response.request.uri.href;
+          if (uri === 'http://www.japanesepod101.com/index.php') {
+            console.log('Log in successful!');
+
+            if (callback !== undefined) {
+              callback();
+            }
+          } else {
+            console.log('Something went wrong. Got redirected to: ' + url);
+          }
+        });
+      } else {
+        console.log('No meta redirect found');
+      }
+    }
+  }
+
+  getPage(url, callback) {
+    request.get({
+      headers: {
+        'User-Agent': this.userAgent,
+      },
+      jar: cookieJar,
+      url: url,
+    }, (error, response, body) => {
+      callback(error, response, body);
+    });
+  }
+  
+  getLoginFormData(something) {
+    // here
+  }
+  
+  login(loginPage, loginData, response, callback) {
+    
+    // move this to getLoginFormData
+    
+    var cookies = response.headers['set-cookie'];
+    var cookieStr = '';
+    var _this = this;
+  
+    for(var i = 0; i < cookies.length; i++) {
+      cookieStr += cookies[i].split(';')[0];
+    }
+    
+    cookieJar.setCookie(cookieStr, '.japanesepod101.com');
+    
+    request.post({
+      followAllRedirects: true,
+      form: loginData,
+      header: {
+        'User-Agent': this.userAgent,
+      },
+      jar: cookieJar,
+      url: loginPage,
+    }, function(error, response, body) {
+      _this.followMetaRedirect(error, response, body, callback);
+    }); 
+  }
+};
+
+module.exports = Jpcrawler;
